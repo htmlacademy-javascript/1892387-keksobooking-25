e219291c647b7b6e830dc2641fa300d2609156fa
OnstopelRebel <72128195+OnstopelRebel@users.noreply.github.com>
2022-03-06 19:56:58 +0300

    Больше деталей правки

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ const getRandomInteger = (x, y) => {
 
 getRandomInteger(1,110);
 
-const getRandomFloat = (x, y, maxDigits = 0) => {
+const getRandomFloat = (x, y, digitsNumber = 0) => {
   if (x < 0 || y < 0) {
     throw new Error('Отрицательный параметр');
   }
-  const digitsDegree = 10 ** maxDigits;
+  const digitsDegree = 10 ** digitsNumber;
   if (x > y) {
     [x, y] = [y, x];
   }
@@ -19,38 +19,46 @@ const getRandomFloat = (x, y, maxDigits = 0) => {
 
 getRandomFloat(1.1234, 50.4321, 3);
 
-const getRandomArray = (arr) => {
-  const maxLength = arr.length;
-  const arrayLength = getRandomInteger(1, maxLength);
-  const array = [];
-  while (array.length < arrayLength) {
-    const elementIndex = getRandomInteger(0, maxLength - 1);
-    const element = arr[elementIndex];
-    if (!array.includes(element)) {
-      array.push(element);
-    }
-  }
-  return array;
-};
+// const getRandomArray = (arr) => {
+//   const maxLength = arr.length;
+//   const arrayLength = getRandomInteger(1, maxLength);
+//   const array = [];
+//   while (array.length < arrayLength) {
+//     const elementIndex = getRandomInteger(0, maxLength - 1);
+//     const element = arr[elementIndex];
+//     if (!array.includes(element)) {
+//       array.push(element);
+//     }
+//   }
+//   return array;
+// };
 
-const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-const CHECKOUT = ['12:00', '13:00', '14:00'];
+const getRandomArray = (initialArray) => initialArray.sort(() => Math.random() - 0.5).slice(0, getRandomInteger(1, initialArray.length));
+
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const CHECKOUTS = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
-const ID = Array.from({length: 10},(v, i) => ++i);
+const cardQuantity = 10;
+const latMin = 35.65000;
+const latMax = 35.70000;
+const lngMin = 139.70000;
+const lngMax = 139.80000;
+const maxDigits = 5;
+const usersId = Array.from({length: cardQuantity},(v, i) => ++i);
 
 const getImageNumber = () => {
-  const imgNumber = ID.splice(0, 1);
+  const imgNumber = usersId.splice(0, 1);
   return imgNumber < 10 ? `0${imgNumber}` : imgNumber;
 };
 
 const createCard = () => {
-  const lat = getRandomFloat(35.65000, 35.70000, 5);
-  const lng = getRandomFloat(139.70000, 139.80000, 5);
+  const lat = getRandomFloat(latMin, latMax, maxDigits);
+  const lng = getRandomFloat(lngMin, lngMax, maxDigits);
   return {
     author: {
       avatar: `img/avatars/user${getImageNumber()}.png`,
@@ -59,22 +67,22 @@ const createCard = () => {
       title: 'ЗАГОЛОВОК ПРЕДЛОЖЕНИЯ',
       address: `${lat}, ${lng}`,
       price: getRandomInteger(1000, 10000),
-      type: TYPE[getRandomInteger(0, 4)],
+      type: TYPES[getRandomInteger(0, 4)],
       rooms: getRandomInteger(1, 5),
       guests: getRandomInteger(1, 6),
-      checkin: CHECKOUT[getRandomInteger(0, 2)],
-      checkout: CHECKOUT[getRandomInteger(0, 2)],
+      checkin: CHECKOUTS[getRandomInteger(0, 2)],
+      checkout: CHECKOUTS[getRandomInteger(0, 2)],
       features: getRandomArray(FEATURES),
       description: 'Очень хорошее помещение',
       photos: getRandomArray(PHOTOS),
     },
     location: {
-      lat: lat,
-      lng: lng,
+      lat,
+      lng,
     }
   };
 };
 
-const createCardsArray = (quantity) => Array.from({length: quantity}, createCard);
+const createCards = (quantity) => Array.from({length: quantity}, createCard);
 
-createCardsArray(10);
+createCards(cardQuantity);

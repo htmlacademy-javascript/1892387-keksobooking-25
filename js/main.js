e92ad4a9cbb21d684fea9c8020680ae26cd04bb9
OnstopelRebel <72128195+OnstopelRebel@users.noreply.github.com>
2022-03-16 19:50:35 +0300

    Отрисуй меня полностью (часть 2)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 import './card_generate.js';
+import './form.js';
+import {formDisabled, formActive} from './form.js';
+formDisabled('.map__filters');
+formDisabled('.ad-form');
+formActive('.ad-form');

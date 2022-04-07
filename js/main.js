a4b6979c39eeada9f9ac4f806f11538b8675aaea
OnstopelRebel <72128195+OnstopelRebel@users.noreply.github.com>
2022-04-07 13:08:54 +0300

    Помощь друга правка2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-import {createCards, createCard} from './data.js';
-import {getGeneratedCard} from './card_generate.js';
-import {CARD_QUANTITY} from './util.js';
 import './form-validator.js';
 import './map.js';
 import './slider.js';
-
-const testCard = createCard();
-
-createCards(CARD_QUANTITY);
-getGeneratedCard(testCard);
